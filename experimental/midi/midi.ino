b17d37ee529b833b7dd39d2dc493dc0c5fdd6dd2
cybernetic_terror <cyb3rnetic@peraltalabs.com>
2013-11-13 10:24:41 -0800

    Got those backwards...

--- experimental/midi/midi.ino
+++ experimental/midi/midi.ino
@@ -4,8 +4,8 @@
   DO NOT MODIFY THIS CODE. Use this as a way to play around with making some noise!
 */
 
-#define MIDI_COMMAND_NOTE_OFF 0x80  // MIDI command to begin playing a note
-#define MIDI_COMMAND_NOTE_ON  0x90  // MIDI command to stop playing a note
+#define MIDI_COMMAND_NOTE_ON  0x80  // MIDI command to stop playing a note
+#define MIDI_COMMAND_NOTE_OFF 0x90  // MIDI command to begin playing a note
 
 #define MIDI_CHANNEL_1 0x00         // MIDI has 16 channels. They go from 0x00 to 0x0F
 
