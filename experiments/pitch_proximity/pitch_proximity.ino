9116627c8a25028144ccdb2ba35f3b81c9f907a3
cybernetic_terror <cyb3rnetic@peraltalabs.com>
2013-11-19 21:36:26 -0800

    Rewrite the numbers so Brian can understand.

--- experiments/pitch_proximity/pitch_proximity.ino
+++ experiments/pitch_proximity/pitch_proximity.ino
@@ -12,15 +12,18 @@
 #define MIDI_CHANNEL_1           0x00  // MIDI has 16 channels. They go from 0x00 to 0x0F
 #define MIDI_MAX_VELOCITY        0x7f  // MIDI velocity, which is how hard you hit the key. Goes from 0x00 to 0x7f
 #define MIDI_COMMAND_PITCH_BEND  0xe0  // MIDI pitch bend command.
-#define MIDI_PITCH_BEND_MID_MSB  0x40  // MSB for no pitch bend
-#define MIDI_PITCH_BEND_MID_LSB  0x00  // LSB for no pitch bend
-#define MIDI_PITCH_BEND_MAX_MSB  0x7f  // MSB for max pitch bend
-#define MIDI_PITCH_BEND_MAX_LSB  0x7f  // LSB for nax pitch bend
+
+#define MIDI_PITCH_BEND_MID_MSB  64  // Coarse stepping "most significant byte" middle amount - no pitch bend
+#define MIDI_PITCH_BEND_MAX_MSB  127 // Coarse stepping "most significant byte" maximum amount - full high pitch bend
+
+#define MIDI_PITCH_BEND_MIN_LSB  0   // Fine stepping "least significant byte" minimum amount - no pitch bend
+#define MIDI_PITCH_BEND_MAX_LSB  127 // Fine stepping "least significant byte" maximum amount - full high pitch bend
 
 #define MIDI_COMMAND_PITCH_BEND_MSB_POS 0 // Position within the MIDI pitch bend command of the MSB
 #define MIDI_COMMAND_PITCH_BEND_LSB_POS 1 // Position within the MIDI pitch bend command of the MSB
 
-#define SENSOR_PIN 0 // Which analog pin is this sensor connected to?
+#define SENSOR_PIN 0   // Which analog pin is this sensor connected to?
+#define SENSOR_MAX 676 // Maximum amount the IR sensor will read
 
 /*
   Number of miliseconds between sensor reaadings. For example, 250ms is 4 times per second.
@@ -34,23 +37,29 @@ int sensorReadInterval = 250;
 byte note = 0x45;
 
 void convertNormalizedPitchToBytes(float normalizedPitch, byte dataBytes[]) {
-  //byte mostSigByte = MIDI_PITCH_BEND_MAX_MSB;
-  byte mostSigByte = (byte)(((float)(MIDI_PITCH_BEND_MAX_MSB - MIDI_PITCH_BEND_MID_MSB)) * normalizedPitch) + MIDI_PITCH_BEND_MID_MSB;
-  byte leastSigByte = MIDI_PITCH_BEND_MID_LSB;
+  // Calculate the MSB: 64 * (value from 0.0 to 1.0, basically a percentage) + 64
+  // This means if the sensor is reading 0, we're at a pitch bend of 64, which is "no change in pitch"
+  // If the sensor is reading 1.0, we're at a pitch bend of 127, which is "max change in pitch"
+  byte mostSigByte  = MIDI_PITCH_BEND_MID_MSB * normalizedPitch + MIDI_PITCH_BEND_MID_MSB;
+  byte leastSigByte = MIDI_PITCH_BEND_MIN_LSB;
   
   dataBytes[MIDI_COMMAND_PITCH_BEND_MSB_POS] = mostSigByte;
   dataBytes[MIDI_COMMAND_PITCH_BEND_LSB_POS] = leastSigByte;
 }
 
+/*
+  Convert the proximity sensor reading into a value from 0.0 to 1.0. Think of this as a percentage of how close
+  the raver's hand is to the sensor. 0.0 being "no one in front" and 1.0 being someone getting allll up in the sensor's business!
+*/
 float calculateNormalizedPitchFromProximity(int proximity) {
-  return (float)proximity / 600.0;
+  return (float)proximity / (float)SENSOR_MAX;
 }
 
 void setup() {
   // MIDI communicates at this baud rate. Do not change!
   Serial.begin(31250);
   
-    // Play a Note
+  // Play a Note
   Serial.write(MIDI_COMMAND_NOTE_ON | MIDI_CHANNEL_1); // 1st Byte: The command. In this case, play note on channel 1.
   Serial.write(note);                                  // 2nd Byte: The note. 
   Serial.write(MIDI_MAX_VELOCITY);                     // 3rd Byte: Velocity ( key press hardness ).    
